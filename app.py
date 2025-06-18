63597b3f86398b4e81f841caa35e8091b273ef08
+ <omnieskay@proton.me>
2025-06-18 20:09:06 +0300

    fix errors

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request,jasonify
+from flask import Flask, request,jsonify
 from flask_cors import CORS
 from flask_migrate import Migrate
 from models import db, User, Workout,Exercise
