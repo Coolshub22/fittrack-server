f882e2acc724c14f4b36e6225bf31e04b44b5608
Arnold <arnoldkulavi@gmail.com>
2025-06-25 13:53:06 +0300

    feat:add serialization and validation in models.py

---
+++ migrations/versions/f15faf60ba81_add_serialisation_and_validation.py
@@ -0,0 +1,24 @@
+"""add serialisation and validation
+
+Revision ID: f15faf60ba81
+Revises: b84b4191b866
+Create Date: 2025-06-25 13:51:24.940807
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'f15faf60ba81'
+down_revision = 'b84b4191b866'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
